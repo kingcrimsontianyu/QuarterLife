307ad424d4d8b1fff95b7d2e396e0c181e61dc61
Tianyu Liu <kingcrimsontianyu@gmail.com>
2021-04-16 23:56:35 -0400

    Added a utility function to handle undesired exponential height fog effect.

--- QL/QLBPUtilityLibrary.cpp
+++ QL/QLBPUtilityLibrary.cpp
@@ -14,13 +14,22 @@
 #include "Engine/World.h"
 #include "QLCharacter.h"
 #include "QLAbilityManager.h"
+#include "Engine/ExponentialHeightFog.h"
+#include "Components/ExponentialHeightFogComponent.h"
 
+//------------------------------------------------------------
+//------------------------------------------------------------
 void UQLBPUtilityLibrary::InitializePlayer(const FString& InitialAbility,
     const FString& InitialWeapon,
     const UObject* WorldContextObj,
     const AActor* NearActor,
     const AActor* FarActor)
 {
+    if (!WorldContextObj)
+    {
+        return;
+    }
+
     ACharacter* MyACharacter = UGameplayStatics::GetPlayerCharacter(WorldContextObj, 0);
 
     if (MyACharacter)
@@ -54,5 +63,50 @@ void UQLBPUtilityLibrary::InitializePlayer(const FString& InitialAbility,
             }
         }
     }
+}
+
+//------------------------------------------------------------
+//------------------------------------------------------------
+void UQLBPUtilityLibrary::HandleExponentialHeightFogPerTick(const UObject* WorldContextObj,
+    const float InitialFogDensity,
+    AExponentialHeightFog* FogActor,
+    const float InterpStartDistance,
+    const float InterpEndDistance)
+{
+    if (!WorldContextObj)
+    {
+        return;
+    }
+
+    ACharacter* MyACharacter = UGameplayStatics::GetPlayerCharacter(WorldContextObj, 0);
+    if (!MyACharacter)
+    {
+        return;
+    }
+
+    float CurrentDistance = FVector::Distance(MyACharacter->GetActorLocation(),
+        FogActor->GetActorLocation());
+
+    if (InterpEndDistance <= InterpStartDistance)
+    {
+        return;
+    }
+
+    // Three cases:
+    // (1) distance between player camera and fog actor is less than FogReductionStartDistance, fog density is unchanged,
+    // (2) distance greater than FogReductionStartDistance and less than FogReductionEndDistance, fog density is interpolated,
+    // (3) distance is greater than FogReductionEndDistance, fog density is 0.
+    float CurrentFogDensity = InitialFogDensity;
+    if (CurrentDistance >= InterpStartDistance && CurrentDistance <= InterpEndDistance)
+    {
+        float alpha = (CurrentDistance - InterpStartDistance) / (InterpEndDistance - InterpStartDistance);
+        CurrentFogDensity = InitialFogDensity * (1.0f - alpha);
+    }
+    else if (CurrentDistance > InterpEndDistance)
+    {
+        CurrentFogDensity = 0.0f;
+    }
+
+    FogActor->GetComponent()->SetFogDensity(CurrentFogDensity);
 }
 
--- QL/QLBPUtilityLibrary.h
+++ QL/QLBPUtilityLibrary.h
@@ -12,8 +12,10 @@
 
 #include "CoreMinimal.h"
 #include "Kismet/BlueprintFunctionLibrary.h"
+
 #include "QLBPUtilityLibrary.generated.h"
 
+class AExponentialHeightFog;
 //------------------------------------------------------------
 //------------------------------------------------------------
 UCLASS()
@@ -27,4 +29,21 @@ class QL_API UQLBPUtilityLibrary : public UBlueprintFunctionLibrary
         const UObject* WorldContextObj,
         const AActor* NearActor = nullptr,
         const AActor* FarActor = nullptr);
+
+    //------------------------------------------------------------
+    // To be used in a special scenario where the level has a sky atmosphere and an exponential height fog.
+    // The fog actor assumes a flat world, and therefore botches the rendering when the player is far from
+    // the earth into the space.
+    // This function makes fog density gradually reduce to 0 when the player is leaving the earth,
+    // or increase to the initial valu;e when the player is approaching the earth.
+    // This function shall be called by the level's Tick() function.
+    // The level shall have a variable to stash the initial value of the fog density,
+    // which is InitialFogDensity.
+    //------------------------------------------------------------
+    UFUNCTION(BlueprintCallable, Category = "C++")
+    static void HandleExponentialHeightFogPerTick(const UObject* WorldContextObj,
+        const float InitialFogDensity,
+        AExponentialHeightFog* FogActor,
+        const float InterpStartDistance,
+        const float InterpEndDistance);
 };
